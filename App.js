1f89c60e00e323ac27040bb999b00997624f89a0
Rayane'CO <81171554+Rayaneko@users.noreply.github.com>
2023-06-21 23:30:41 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Navbar from "././components/Navbar/Navbar"
+import Planche from "../src/components/pages/Planche"
+import Home from "../src/components/pages/Home"
+import About from "../src/components/pages/About"
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    
+      <Navbar />
+      
+  )
+}
+
+export default App
